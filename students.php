12e3a3f29841edffefadd39fab8b4184e77512b6
Captaincruda25 <crudacarlemmanuelt25@gmail.com>
2024-08-01 13:29:11 +0800

    fixed database and register function

--- students.php
+++ students.php
@@ -18,16 +18,17 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Intern Records</title>
     <link rel="stylesheet" href="css/students.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
     <div class="container">
         <nav class="sidebar">
             <div class="logo">EACMed</div>
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Interns</a></li>
-                <li><a href="#">Analytics</a></li>
-                <li><a href="#">Reports</a></li>
+                <li><a href="dashboard.php"><i class="fas fa-home"></i> Home</a></li>
+                <li><a href="students.php"><i class="fas fa-users"></i> Interns</a></li>
+                <li><a href="#"><i class="fas fa-chart-line"></i> Attendance</a></li>
+                <li><a href="#"><i class="fas fa-file-alt"></i> Reports</a></li>
             </ul>
         </nav>
         <main class="main-content">
@@ -37,7 +38,10 @@ session_start();
             </header>
             <div class="filter">
                 <form action="students.php" method="POST">
-                    <input type="text" name="search" placeholder="Search...">
+                    <div class="search-bar">
+                        <i class="fas fa-search"></i>
+                        <input type="text" name="search" placeholder="Search...">
+                    </div>
                     <button type="submit" name="btn">Filter Results</button>
                 </form>
             </div>
@@ -51,76 +55,73 @@ session_start();
                         <th>Age</th>
                         <th>Started Date</th>
                         <th>End Date</th>
+                        <th>Overall Rendered Hours</th>
                         <th>Overall Remaining Hours</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
-
-
                         if(isset($_POST['btn'])){
                             $search = $_POST['search'];
 
-                            $find = "SELECT * FROM studentinfo WHERE id LIKE '%$search%' OR fname LIKE '%$search%' OR mname LIKE '%$search%' OR lname LIKE '%$search' OR age LIKE '%$search%' OR sex LIKE '%$search%' OR course LIKE '%$search%';";
+                            $find = "SELECT * FROM studentinfo WHERE id LIKE '%$search%' OR fname LIKE '%$search%' OR mname LIKE '%$search%' OR lname LIKE '%$search%' OR age LIKE '%$search%' OR sex LIKE '%$search%' OR course LIKE '%$search%' OR school LIKE '%$search%';";
                             $searchquery = mysqli_query($conn, $find);
                             $exist = mysqli_num_rows($searchquery);
 
                             if($exist > 0){
                                 while($row = mysqli_fetch_assoc($searchquery)){
                                     $fname  = $row['fname'];
-                                    $mname  = ($row['mname']);
+                                    $mname  = $row['mname'];
                                     $lname  = $row['lname'];
                                     $course = $row['course'];
                                     $sex = $row['sex'];
                                     $age = $row['age'];
+                                    $schoolname = $row['school'];
                                     
                                     echo "<tr class='highlight'>";
                                     echo "<td>" .$lname. "," .$fname. " " .$mname. "</td>";
-                                    echo "<td></td>";
+                                    echo "<td>" .$schoolname. "</td>";
                                     echo "<td>" .$course. "</td>";
                                     echo "<td>" .$sex. "</td>";
                                     echo "<td>" .$age. "</td>";
+                                    echo "<td></td>";
+                                    echo "<td></td>";
+                                    echo "<td></td>";
+                                    echo "<td></td>";
                                     echo "</tr>";
                                 }
                             }
                             else {
-                                echo "<td colspan='8' style='text-align:center'> No Data Found ..... </td>";
+                                echo "<td colspan='9' style='text-align:center'> No Data Found ..... </td>";
                             }
                         }
                         else {
-                            $sql = "SELECT * FROM studentinfo";
+                            $sql = "SELECT * FROM studentinfo INNER JOIN school ON studentinfo.schoolid = school.id";
                             $query = mysqli_query($conn, $sql);
 
                             while($row = mysqli_fetch_assoc($query)){
                                 $fname  = $row['fname'];
-                                $mname  = ($row['mname']);
+                                $mname  = $row['mname'];
                                 $lname  = $row['lname'];
-                                $course = $row['course'];
+                                // $course = $row['course'];
                                 $sex = $row['sex'];
                                 $age = $row['age'];
-                                
-                                // $school = $row['school']; 
+                                $school = $row['schoolname'];
+
                                 echo "<tr class='highlight'>";
                                 echo "<td>" .$lname. "," .$fname. " " .$mname. "</td>";
+                                echo "<td>" .$school. "</td>";
                                 echo "<td></td>";
-                                echo "<td>" .$course. "</td>";
                                 echo "<td>" .$sex. "</td>";
                                 echo "<td>" .$age. "</td>";
+                                echo "<td></td>";
+                                echo "<td></td>";
+                                echo "<td></td>";
+                                echo "<td></td>";
                                 echo "</tr>";
-                                // echo "<td>" .. "</td>";
-                                // // echo "<td>"  "</td>";
-                                // // echo "<td>"  "</td>";
                             }
                         }
-                        
-                        
-
-
-
                     ?>
-                        
-                    
-                    <!-- Add more rows as needed -->
                 </tbody>
             </table>
         </main>

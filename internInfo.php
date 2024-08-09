3135b0d7556fe5c383cb2acea07cd0e4d86eeccf
CaptainCruda25 <crudacarlemmanuelt25@gmail.com>
2024-08-09 11:14:24 +0800

    fixed General UI

---
+++ internInfo.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intern Records</title>
+    <link rel="stylesheet" href="css/internInfo.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="icon" type="png" href="img/logo-icon.png">
+</head>
+<body>
+    <div class="container">
+        <nav class="sidebar">
+            <ul>
+                <div class="logo">
+                    <img src="img/logo-icon.png" alt="EACMed Logo">
+                </div>
+                <li><a href="dashboard.php"><i class="fas fa-home"></i> <span>Home</span></a></li>
+                <li><a href="students.php"><i class="fas fa-users"></i> <span>Interns</span></a></li>
+                <li><a href="attendance.php"><i class="fas fa-chart-line"></i> <span>Attendance</span></a></li>
+            </ul>
+            <div class="logout-container">
+                <button class="Btn">
+                    <div class="sign">
+                        <svg viewBox="0 0 512 512">
+                            <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path>
+                        </svg>
+                    </div>
+                    <div class="text">Logout</div>
+                </button>
+            </div>
+        </nav>
+        <div class="main-content">
+    <div class="intern-info">
+        <div class="profile">
+            <img src="img/kapatidnijoanna.jpg" alt="Profile Picture">
+            <h2>Kapatid ni Joanna</h2>
+            <p>Cavite State University - Naic</p>
+        </div>
+        <div class="details">
+    <div class="detail">Name: Mark Joven A. Garcia</div>
+    <div class="detail">BirthDate: 04/05/03</div>
+    <div class="detail">Age: 21</div>
+    <div class="detail">Address: Timalan Conception Naic, Cavite</div>
+    <div class="detail">Contact No: 093564457486</div>
+    <div class="detail">Guardian: Maryln A. Garcia</div>
+    <div class="detail">Parent Contact No: 093564457486</div>
+</div>
+
+        <div class="logo">
+            <img src="img/EACMedOnly.png" alt="EACMed Logo">
+        </div>
+    </div>
+    <div class="attendance-record">
+        <div class="table-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Time in</th>
+                        <th>Time Out</th>
+                        <th>Rendered Time</th>
+                        <th>Remaining Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>07-22-24</td>
+                        <td>7:36 AM</td>
+                        <td>5:12 PM</td>
+                        <td>Complete</td>
+                        <td>232 Hours</td>
+                    </tr>
+                    <tr>
+                        <td>07-23-24</td>
+                        <td>7:38 AM</td>
+                        <td>5:16 PM</td>
+                        <td>Complete</td>
+                        <td>232 Hours</td>
+                    </tr>
+                    <tr>
+                        <td>07-24-24</td>
+                        <td>7:51 AM</td>
+                        <td>5:38 PM</td>
+                        <td>Complete</td>
+                        <td>232 Hours</td>
+                    </tr>
+                    <tr>
+                        <td>07-25-24</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>232 Hours</td>
+                    </tr>
+                    <tr>
+                        <td>07-26-24</td>
+                        <td>7:58 AM</td>
+                        <td>5:29 PM</td>
+                        <td>Complete</td>
+                        <td>232 Hours</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="attendance-buttons">
+            <button class="btn-time">Time In</button>
+            <button class="btn-time2">Time Out</button>
+        </div>
+    </div>
+</div>
+
+
+</div>
+
+    </div>
+</body>
+</html>
